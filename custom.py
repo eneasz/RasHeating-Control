96c9a58330efc413a514170714c33285fcf945fb
eneasz <eneasz@eneasz.aol.com>
2013-12-13 17:34:07 +0000

    further work on scheduler

---
+++ custom.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+week={
+
+        'Monday':[{'hour': 10, 'minute': 20, 'duration': 0},
+                {'hour': 12, 'minute': 20, 'duration': 2},
+                {'hour': 15, 'minute': 20, 'duration': 2},
+                {'hour': 20, 'minute': 30, 'duration': 20},
+                ],
+
+        'Tuesday':[{'hour': 10, 'minute': 20, 'duration': 0},
+                {'hour': 12, 'minute': 20, 'duration': 2},
+                {'hour': 15, 'minute': 20, 'duration': 2},
+                {'hour': 20, 'minute': 30, 'duration': 20},
+                ],
+
+        'Wednesday':[{'hour': 10, 'minute': 20, 'duration': 0},
+                {'hour': 12, 'minute': 20, 'duration': 2},
+                {'hour': 15, 'minute': 20, 'duration': 2},
+                {'hour': 20, 'minute': 30, 'duration': 20},
+                ],
+
+        'Thursday':[{'hour': 10, 'minute': 20, 'duration': 0},
+                {'hour': 12, 'minute': 20, 'duration': 2},
+                {'hour': 15, 'minute': 20, 'duration': 2},
+                {'hour': 20, 'minute': 30, 'duration': 20},
+                ],
+
+	'Friday':[{'hour': 10, 'minute': 20, 'duration': 0},
+                {'hour': 12, 'minute': 20, 'duration': 2},
+                {'hour': 17, 'minute': 34, 'duration': 1},
+		{'hour': 20, 'minute': 30, 'duration': 20},
+                ],
+
+	'Saturday':[{'hour': '15:00', 'duration': 5},
+		{'hour': 20, 'minute': 30, 'duration': 20},
+		],
+
+	'Sunday':[{'hour': '15:00', 'duration': 5},
+                {'hour': 20, 'minute': 30, 'duration': 20},
+                ]
+}
--- scheduler.py
+++ scheduler.py
@@ -1,57 +1,24 @@
+#!/usr/bin/python
+
+#Importing stuff including scheduler-custom which is a file where you define your own scheduler
+#sys.path.append('/Users/eneasz/python/RasHeating-Control')
 from datetime import date
+import custom
 import datetime, time
+
+#Define how to find out hour, minute and day of the week
 hour = datetime.datetime.now().hour
 minute = datetime.datetime.now().minute
-oldDate = date(datetime.datetime.today().year,datetime.datetime.today().month,datetime.datetime.today().day) # year, month, day
 dayofWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-#day2=dayofWeek[date.weekday(oldDate)]
-week={
-
-        'Monday':[{'hour': 10, 'minute': 20, 'duration': 0},
-                {'hour': 12, 'minute': 20, 'duration': 2},
-                {'hour': 15, 'minute': 20, 'duration': 2},
-                {'hour': 20, 'minute': 30, 'duration': 20},
-                ],
-
-        'Tuesday':[{'hour': 10, 'minute': 20, 'duration': 0},
-                {'hour': 12, 'minute': 20, 'duration': 2},
-                {'hour': 15, 'minute': 20, 'duration': 2},
-                {'hour': 20, 'minute': 30, 'duration': 20},
-                ],
-
-        'Wednesday':[{'hour': 10, 'minute': 20, 'duration': 0},
-                {'hour': 12, 'minute': 20, 'duration': 2},
-                {'hour': 15, 'minute': 20, 'duration': 2},
-                {'hour': 20, 'minute': 30, 'duration': 20},
-                ],
-
-        'Thursday':[{'hour': 10, 'minute': 20, 'duration': 0},
-                {'hour': 12, 'minute': 20, 'duration': 2},
-                {'hour': 15, 'minute': 20, 'duration': 2},
-                {'hour': 20, 'minute': 30, 'duration': 20},
-                ],
-
-	'Friday':[{'hour': 10, 'minute': 20, 'duration': 0},
-                {'hour': 12, 'minute': 20, 'duration': 2},
-                {'hour': 15, 'minute': 36, 'duration': 1},
-		{'hour': 20, 'minute': 30, 'duration': 20},
-                ],
-
-	'Saturday':[{'hour': '15:00', 'duration': 5},
-		{'hour': 20, 'minute': 30, 'duration': 20},
-		],
-
-	'Sunday':[{'hour': '15:00', 'duration': 5},
-                {'hour': 20, 'minute': 30, 'duration': 20},
-                ]}
-
-
+oldDate = date(datetime.datetime.today().year,datetime.datetime.today().month,datetime.datetime.today().day) # year, month, day
 today = dayofWeek[date.weekday(oldDate)]
-print "Today is %s in simple words it\'s %s" % (oldDate.strftime("%d%b%Y"), dayofWeek[date.weekday(oldDate)])
+
+
+print "Today is %s %s" % (dayofWeek[date.weekday(oldDate)], (oldDate.strftime("%d%b%Y")))
 for s in dayofWeek:
     if s==today:
         print (s + " Winner!!")
-        for instance in week[s]:
+        for instance in custom.week[s]:
 		if hour == instance['hour'] and minute == instance['minute']: 
 	            print instance['hour'], instance['minute']
 		    print("This is working")

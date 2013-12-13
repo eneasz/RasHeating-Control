4d0b813b6d96073cc6bafe9abde591fbe8350c65
eneasz <eneasz@eneasz.aol.com>
2013-12-13 17:40:19 +0000

    further work on scheduler

--- scheduler.py
+++ scheduler.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python
 
 #Importing stuff including scheduler-custom which is a file where you define your own scheduler
-#sys.path.append('/Users/eneasz/python/RasHeating-Control')
-from datetime import date
 import custom
 import datetime, time
 
@@ -10,11 +8,11 @@ import datetime, time
 hour = datetime.datetime.now().hour
 minute = datetime.datetime.now().minute
 dayofWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-oldDate = date(datetime.datetime.today().year,datetime.datetime.today().month,datetime.datetime.today().day) # year, month, day
-today = dayofWeek[date.weekday(oldDate)]
+oldDate = datetime.date(datetime.datetime.today().year,datetime.datetime.today().month,datetime.datetime.today().day) # year, month, day
+today = dayofWeek[datetime.date.weekday(oldDate)]
 
 
-print "Today is %s %s" % (dayofWeek[date.weekday(oldDate)], (oldDate.strftime("%d%b%Y")))
+print "Today is %s %s" % (dayofWeek[datetime.date.weekday(oldDate)], (oldDate.strftime("%d%b%Y")))
 for s in dayofWeek:
     if s==today:
         print (s + " Winner!!")

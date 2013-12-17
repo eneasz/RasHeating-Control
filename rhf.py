cfcb4abeb5a61669ef1baad813296cc63399beb0
eneasz <eneasz@aol.com>
2013-12-17 21:42:56 +0000

    added blinking LED, some code cleaning

--- rhf.py
+++ rhf.py
@@ -1,27 +1,46 @@
 #!/usr/bin/python
-#Setting up current time
-import datetime
+#Importing modules
+import datetime, os, sys, time, glob
+from datetime import datetime as t
+import RPi.GPIO as GPIO ## Import GPIO library
+
+
 
 #Define action
-def ogrzewanie(STATE):
+def heating_status(STATE):
         if STATE!=False and STATE!=True :
               STATE=False
               print STATE
+
 def run_heating(STATE) :
-#Import stuff
-   import os, time, sys, glob
-   from datetime import datetime as t
-   
    #Checking if we already are root and if not reloading with sudo.
    #Thanks to https://gist.github.com/davejamesmiller/1965559
    if os.geteuid() != 0:
-   	os.execvp("sudo", ["sudo"] + sys.argv)
+        os.execvp("sudo", ["sudo"] + sys.argv)
+
    
-   #Temp MIN
-   TSET = 29
-   TMARG = 5
+   #Settings 
+   TSET = 17.15 #Tempe max
+   TMARG = 0.5 # TSET - TMARG = Minimal temperature below which heating will be enabled
    TEMP_SCALE="C" # Show temperature in C or F
-   #status
+   LED=True # Set to True or False to enable or disable LED
+
+   #Setting GPIO controling LED - ON  when heating is running, OFF when it's not running
+   GPIO.setwarnings(False)
+   GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
+   GPIO.setup(11, GPIO.OUT) ## Setup GPIO Pin 11 to OUT
+
+
+   if LED==True:
+        SPEED = 0.2 #Speed how quickly LED is blinking
+        NUMTIMES = 3 #How many time blink in each sequence
+
+        def Blink(speed):
+                for i in range(0,NUMTIMES):## Run loop NUMTIMES
+                 GPIO.output(11,True)## Switch on pin 11
+                 time.sleep(speed)## Wait
+                 GPIO.output(11,False)## Switch off pin 11
+                 time.sleep(speed)## Wait
 
    #Set sensor
    os.system('modprobe w1-gpio')
@@ -37,6 +56,10 @@ def run_heating(STATE) :
            lines = f.readlines()
            f.close()
            return lines
+
+   def curent():
+        curent_temp = tmp
+        return(curent_temp)
    
    #Def read temperature with nice conversation
    def read_temp():
@@ -79,7 +102,7 @@ def run_heating(STATE) :
 	int(tmp)
    except:
    	print color.FAIL + ("Temperature value is not a number") + color.ENDC
-   	ogrzewanie("False")
+   	heating_status("False")
    
    
    #Making decision weather to switch on/off
@@ -87,21 +110,25 @@ def run_heating(STATE) :
    if tmp >= TSET and (STATE==None or STATE==True):
    	STATE = False
    	print color.OKGREEN + ("Temperature " + str(tmp) + " " + str(TEMP_SCALE) + " :Reached MAX, switching OFF heating") + color.ENDC
+        if LED==True: GPIO.output(11,False) ## Turn off GPIO pin 11
 #tmp >= tset just being off
    elif tmp >= TSET and (STATE==False):
    	print color.OKGREEN + ("Temperature " + str(tmp) + " " + str(TEMP_SCALE) + " :We don't need to runn the heating") + color.ENDC
+	if LED==True: GPIO.output(11,False) ## Turn off GPIO pin 11
 #tmp<tset status is true and heating is running
    elif tmp<TSET and (STATE==True):
    	print color.BLUE + ("It\'s " + str(tmp) + " " + str(TEMP_SCALE) + " :Heating is running now") + color.ENDC
+	if LED==True: Blink(float(SPEED))
 #tmp<tset-tmarg switching on
    elif tmp < TSET - TMARG and (STATE==None or not STATE or STATE==False):
    	STATE = True
    	print color.BLUE + ("Iti\'s " + str(tmp) + " " + str(TEMP_SCALE) + " :Switching ON heating") + color.ENDC
+	if LED==True: Blink(float(SPEED))
 #tset-tmarg<=tmp 
    elif tmp >=TSET-TMARG  and STATE==False:
    	print color.WARNING + ("Temperature " + str(tmp) + " " + str(TEMP_SCALE) + " :Within set range, no change required") + color.ENDC
+	if LED==True: GPIO.output(11,False) ## Turn off GPIO pin 11
    
-   #Executing function ogrzewanie
-   ogrzewanie(STATE);
-   time.sleep(2)
+   heating_status(STATE);
+   time.sleep(1)
    return STATE

71f671a0f55844c9a0276612c362254508de5e21
eneasz <eneasz@aol.com>
2014-01-18 19:49:43 +0000

    Cleaning up code and fixed a bug, now scheduler will find out if it's in the middle of the cycle and will enable task rather than wait till next start cycle

--- scheduler.py
+++ scheduler.py
@@ -5,7 +5,8 @@
 #================================================================================================
 
 #Importing stuff including scheduler-custom which is a file where you define your own scheduler
-import sys, os, datetime, time, rhf
+import sys, os, time, rhf
+from datetime import datetime, date, timedelta
 
 _scheduler="custom.py"
 if ( not os.path.isfile(_scheduler)):
@@ -14,14 +15,9 @@ if ( not os.path.isfile(_scheduler)):
 else:
     import custom
 
-#Defined dayofweek, today's date and resetting  end and running counters
-DAYOFWEEK = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-OLDDATE = datetime.date(datetime.datetime.today().year,datetime.datetime.today().month,datetime.datetime.today().day) # year, month, day
-TODAY = DAYOFWEEK[datetime.date.weekday(OLDDATE)]
-END = datetime.datetime.now()
+#List of Days in week
+day_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 RUNNING = False
-STATE = False
-#global STATE
 
 
 while True:
@@ -29,30 +25,29 @@ while True:
  reload(custom)
 
 ##Setting up current time
- HOUR = datetime.datetime.now().hour
- MINUTE = datetime.datetime.now().minute
+ curently = timedelta(hours=datetime.now().hour, minutes=datetime.now().minute)
 
-#Checking for each day of the week in search of Today
-# for SCHEDULER in DAYOFWEEK:
-#     if SCHEDULER==TODAY and (RUNNING==False or RUNNING==None):
- for instance in custom.week[TODAY]:
-	  if (HOUR==instance['hour'] and MINUTE==instance['minute'] and RUNNING==False):
-            start = datetime.datetime.now()
-            END = start + datetime.timedelta(minutes=instance['duration'])
-	    print("Enabling scheduler : " + str(instance['hour']) + ":" + str(instance['minute']) + ". This will finish at " + str(END))
+#Checking each event of Today's 
+ for event in custom.week[day_of_week[date.weekday(date.today())]]:
+     if RUNNING==False:
+	start = timedelta(hours=event['hour'], minutes=event['minute'])
+	finish = start + timedelta(minutes=event['duration'])
+
+	if (start < curently < finish):
+	    print("Running scheduler : " + str(event['hour']) + ":" + str(event['minute']) + ".00" + ". This will finish at " + str(finish))
 	    RUNNING=True
-	    STATE=False
 	    break
 
-#Executing scheduled code when time arrived.
- if datetime.datetime.now() < END and RUNNING==True:
-#			print("<= This is where your code gets executed at scheduler time =>")
-			STATE = rhf.run_heating(STATE)
-
 #When time run out, finishing task
- if datetime.datetime.now() > END and RUNNING==True:
-	RUNNING=False
-	STATE=False
-	rhf.heating_status("False")
-	print("Scheduled task finished")
+ if RUNNING==True:
+	if curently >= finish:
+            RUNNING=False
+            rhf.heating_status("False")
+            print("Scheduled task finished")
+
+#Executing scheduled code when time arrived.
+	if curently < finish:
+#	    print("<= This is where your code gets executed at scheduler time =>")
+	    STATE = rhf.run_heating(True)
+
  time.sleep(3)
